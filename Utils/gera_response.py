7ea9626779fc9f7502c2ec4ff24bad47f4e1a30f
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-13 15:33:52 -0300

    Update gera_response.py

--- Utils/gera_response.py
+++ Utils/gera_response.py
@@ -5,14 +5,14 @@ import json
 def gera_response(status, content_index: str, content: dict, message=None):
     """
     Exemplos:
-    1# return gera_response(200, "cadastro-provas", dict_values, "ok")
-    2# return gera_response(400, "cadastro-provas", {}, "chave inválida")
+    1# return gera_response(200, "user", dict_values, "ok")
+    2# return gera_response(400, "users", {}, "invalid key")
 
     :param status: HTTP Status
-    :param content_index: nome do conteudo
-    :param content: dicionario com dados
-    :param message: "ok", "erro..."
-    :return: json com tudo.
+    :param content_index: content name
+    :param content: dict with data
+    :param message: "ok", "error..."
+    :return: Response
     """
     body = {content_index: content}
     if message:
