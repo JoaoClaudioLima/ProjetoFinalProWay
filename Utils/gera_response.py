79eca4d656c497881e53b4d4b0a614457968f3d2
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-13 15:32:08 -0300

    Update gera_response.py

--- Utils/gera_response.py
+++ Utils/gera_response.py
@@ -2,20 +2,20 @@ from flask import Response
 import json
 
 
-def gera_response(status, nome_conteudo, conteudo, mensagem=None):
+def gera_response(status, content_index: str, content: dict, message=None):
     """
     Exemplos:
     1# return gera_response(200, "cadastro-provas", dict_values, "ok")
     2# return gera_response(400, "cadastro-provas", {}, "chave inválida")
 
     :param status: HTTP Status
-    :param nome_conteudo: nome do conteudo
-    :param conteudo: dicionario com dados
-    :param mensagem: "ok", "erro..."
+    :param content_index: nome do conteudo
+    :param content: dicionario com dados
+    :param message: "ok", "erro..."
     :return: json com tudo.
     """
-    body = {nome_conteudo: conteudo}
-    if mensagem:
-        body["mensagem"] = mensagem
+    body = {content_index: content}
+    if message:
+        body["message"] = message
 
     return Response(json.dumps(body), status=status, mimetype="application/json")
