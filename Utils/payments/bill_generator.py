d0d41576a71b7a544766069b071c22a8f6f4bda8
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-13 11:30:25 -0300

    Update bill_generator.py

---
+++ Utils/payments/bill_generator.py
@@ -0,0 +1,18 @@
+import json
+import datetime
+import uuid
+
+BANK_NUMBER = "237"
+
+
+def generate_bill(payment_data: dict) -> dict:
+    bill = {}
+    bill['due_date'] = str(datetime.date.today() + datetime.timedelta(days=3))
+    bill['documment_date'] = str(datetime.date.today())
+    bill['value'] = payment_data['total']
+    bill['barcode'] = f"{BANK_NUMBER}00.{datetime.datetime.now().strftime('%m%d%Y.%H%M%S')} {str(uuid.uuid4().int).replace('-', '')}"
+
+    return bill
+
+# print(generate_bill({"total": 34.5}))
+
