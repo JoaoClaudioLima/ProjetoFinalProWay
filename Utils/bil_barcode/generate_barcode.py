6dfcf4c8cf06ea4c79d24f2dcd4f398775df0deb
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-13 16:30:37 -0300

    Update generate_barcode.py

--- Utils/bil_barcode/generate_barcode.py
+++ Utils/bil_barcode/generate_barcode.py
@@ -10,6 +10,3 @@ def generate_barcode(barcode: str):
     number = str(barcode).strip()
     my_code = Code128(number)
     my_code.save(str(barcode).strip())
-
-
-# generate_barcode("23700.99999 05132021.125047 213931361184089670357388609302949910791")
