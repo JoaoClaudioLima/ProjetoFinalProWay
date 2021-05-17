3ec1c6e9e44059058ba75a6472cd2476e4c12446
João Claudio Bastos Lima <77875291+JoaoClaudioLima@users.noreply.github.com>
2021-05-17 12:38:20 -0300

    Test Calculate Shipment
    Done

---
+++ tests/test_calculate_shipment.py
@@ -0,0 +1,17 @@
+from unittest import TestCase, mock
+from Utils.shipment.calculate_shipment import calculate_shipments
+
+
+class TestCalculateShipment(TestCase):
+
+    def test_calculate_shipment_works(self):
+        read_return = """{"RO": 50.0, "AC": 5000.0, "RM": 50.0}"""
+        with mock.patch("Utils.shipment.calculate_shipment.open", mock.mock_open(read_data=read_return)):
+
+            dic = dict(checkout=dict(user=dict(address=dict(address_state="RO"))))
+            value = 50.0
+            self.assertEqual(calculate_shipments(dic), value)
+
+        with mock.patch("Utils.shipment.calculate_shipment.json") as mock_json:
+            delattr(mock_json, "loads")
+            self.assertEqual(calculate_shipments({}), "loads")
