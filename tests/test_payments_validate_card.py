92f63f3832a6524f468aa680197fdac97afd757b
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-17 08:25:39 -0300

    finished tests

--- tests/test_payments_validate_card.py
+++ tests/test_payments_validate_card.py
@@ -26,24 +26,24 @@ class TestValidateCard(TestCase):
         expected_return = {'message': 'Card is expired', 'status': False}
         self.assertEqual(expected_return, card_validating(card_info=test_card))
 
+    # @mock.patch("Utils.payments.validate_card.data")
     def test_confirm_payment_works(self):
         read_file = json.dumps({
-            "cards": [{"number": 55,
-                      "month": "05",
-                      "year": "2021",
-                      "cvc": 336,
-                      "credit_limit": 500,
-                      "credit_used": 0,
-                      "debit": 400},
-                      {"number": 44,
+            "cards": [{"number": "34",
                        "month": "04",
                        "year": "2021",
-                       "cvc": 336,
+                       "cvc": "336",
+                       "credit_limit": 500,
+                       "credit_used": 0,
+                       "debit": 400},
+                      {"number": "5450899060743787",
+                       "month": "05",
+                       "year": "2021",
+                       "cvc": "336",
                        "credit_limit": 500,
                        "credit_used": 0,
                        "debit": 400}]
         })
-
         mock_open = mock.mock_open(read_data=read_file)
         with mock.patch("Utils.payments.validate_card.open", mock_open):
             test_card = dict(number="344604579207048",
@@ -52,14 +52,32 @@ class TestValidateCard(TestCase):
                              cvc="336")
             expected_return = {'message': 'operation not allowed', 'status': False}
             self.assertEqual(expected_return, confirm_payment(informed_card=test_card,
-                                                method='credit',
-                                                value=999))
+                                                              method='credit',
+                                                              value=999))
 
-            test_card = dict(number="44",
-                             month="04",
+            test_card = dict(number="5450899060743787",
+                             month="05",
                              year="2021",
                              cvc="336")
-            expected_return = 0
+            expected_return = {'message': 'ok', 'status': True}
             self.assertEqual(expected_return, confirm_payment(informed_card=test_card,
                                                               method='credit',
                                                               value=1))
+
+            self.assertEqual(expected_return, confirm_payment(informed_card=test_card,
+                                                              method='debit',
+                                                              value=1))
+
+            test_card = dict(number="5450899060743787",
+                             month="05",
+                             year="2021",
+                             cvc="336")
+            expected_return = {'message': 'insufficient credit balance', 'status': False}
+            self.assertEqual(expected_return, confirm_payment(informed_card=test_card,
+                                                              method='credit',
+                                                              value=600))
+
+            expected_return = {'message': 'insufficient debit balance', 'status': False}
+            self.assertEqual(expected_return, confirm_payment(informed_card=test_card,
+                                                              method='debit',
+                                                              value=600))
