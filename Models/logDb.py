060a436c603dc5a8eaa094431fbcf37b46d541c8
João Claudio Bastos Lima <77875291+JoaoClaudioLima@users.noreply.github.com>
2021-05-20 09:50:22 -0300

    Ajuste de rota do cluster

--- Models/logDb.py
+++ Models/logDb.py
@@ -4,6 +4,6 @@ import pymongo
 class LogConnectionMongo:
 
     def __init__(self):
-        self.conn = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.conn = pymongo.MongoClient("mongodb+srv://Jao:jaojao@cluster0.hb5l7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         self.db = self.conn["ecommerceDb"]
         self.log = self.db["log_vendas"]
