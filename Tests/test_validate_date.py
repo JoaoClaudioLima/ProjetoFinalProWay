b54cbbbd53c33e074f18182ed3e6d7c7f5da2543
Bruno Bastos <brunomsb64@gmail.com>
2021-05-25 13:32:02 -0300

    Correção do teste do valida data

--- Tests/test_validate_date.py
+++ Tests/test_validate_date.py
@@ -1,3 +1,4 @@
+import datetime
 from unittest import TestCase
 
 from Utils.Process_order.validate_date import validate_date
@@ -6,11 +7,14 @@ from Utils.Process_order.validate_date import validate_date
 class TestValidateDate(TestCase):
 
     def test_get_product(self):
-        initial_date, final_date  = validate_date("2000-01-01", "2021-05-24")
+        to_day = str(datetime.date.today())
+
+        initial_date, final_date = validate_date("2000-01-01", to_day)
         self.assertEqual(str(initial_date), "2000-01-01 00:00:00")
-        self.assertEqual(str(final_date), "2021-05-24 00:00:00")
+        self.assertEqual(str(final_date), f"{to_day} 00:00:00")
 
         initial_date, final_date = validate_date(None, None)
         self.assertEqual(str(initial_date), "2000-01-01 00:00:00")
-        self.assertEqual(str(final_date), "2021-05-24 00:00:00")
+        self.assertEqual(str(final_date), f"{to_day} 00:00:00")
+
 
