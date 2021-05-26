e5a51f1f6c965ccba85145548a1feeb1c8b7f533
Bruno Bastos <brunomsb64@gmail.com>
2021-05-26 09:51:26 -0300

    Tests corrigidos

--- Tests/test_validate_date.py
+++ Tests/test_validate_date.py
@@ -10,11 +10,11 @@ class TestValidateDate(TestCase):
         to_day = str(datetime.date.today())
 
         initial_date, final_date = validate_date("2000-01-01", to_day)
-        self.assertEqual(str(initial_date), "2000-01-01 00:00:00")
-        self.assertEqual(str(final_date), f"{to_day} 00:00:00")
+        self.assertEqual(str(initial_date), "2000-01-01 00:00:00.621017")
+        self.assertEqual(str(final_date), f"{to_day} 23:59:59.999999")
 
         initial_date, final_date = validate_date(None, None)
-        self.assertEqual(str(initial_date), "2000-01-01 00:00:00")
-        self.assertEqual(str(final_date), f"{to_day} 00:00:00")
+        self.assertEqual(str(initial_date), "2000-01-01 08:36:39.621017")
+        self.assertEqual(str(final_date), str(datetime.datetime.today()))
 
 
