adb0ea553298752b5a27c602a843cd288edd66ab
Guilherme Peyerl Florêncio <60821255+GuilhermePeyflo@users.noreply.github.com>
2021-05-26 10:21:02 -0300

    atualização do riqueriments

--- requeriments.bat
+++ requeriments.bat
@@ -1,5 +1,6 @@
 cd Documents\ProjetoFinalProWay
 
+
 pip3 install captain-pycard
 pip3 install flask
 pip3 install flask-restful
@@ -8,5 +9,8 @@ pip3 install pymongo
 pip3 install pandas
 pip3 install requests
 pip3 install pytest-cov
-
-
+pip3 install cryptography
+pip3 install dnspython
+pip3 install reportlab
+pip3 install pytest
+pip3 install coverage
